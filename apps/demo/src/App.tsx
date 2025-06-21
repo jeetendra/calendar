1c29a1fb4b09e282f254d568901889ab8717b4dc
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-06-21 15:14:40 +0530

    created base project

---
+++ apps/demo/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@js/component";
+
+function App() {
+  return (
+    <>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/component/src/index.ts
@@ -0,0 +1,2 @@
+export { default as Button } from "./Button.tsx";
+export { default as Card } from "./Card.tsx";
